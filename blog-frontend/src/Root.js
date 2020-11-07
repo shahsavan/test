c8e2737ea90eff046918c4bc06a4d34aefe2412e
Saeed <s.shahsavan@gmail.com>
2020-11-07 14:35:42 +0330

    initial Version

---
+++ blog-frontend/src/Root.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import App from 'components/App';
+import { Provider } from 'react-redux';
+import configure from 'store/configure';
+import { Route } from 'react-router-dom';
+import {Form, FormText} from "reactstrap";
+
+const store = configure();
+
+const Root = () => {
+  return (
+      <Provider store={store}>
+      <BrowserRouter>
+        <Route path="/" component={App} />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default Root;
